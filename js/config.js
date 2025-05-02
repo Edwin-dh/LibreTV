18c181fc98838a4e224b24918b0ace5467415d63
Edwin-dh <76695570+Edwin-dh@users.noreply.github.com>
2025-05-03 05:53:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -23,7 +23,7 @@ const SITE_CONFIG = {
 const API_SITES = {
     heimuer: {
         api: 'https://json.heimuer.xyz',
-        name: '黑木耳',
+        name: '黑画',
         detail: 'https://heimuer.tv',
         filterAdRule: '#EXT-X-DISCONTINUITY\\n#EXTINF:\\d+\\.\\d+,\\n.*?\\n#EXT-X-DISCONTINUITY'
     },
@@ -51,6 +51,10 @@ const API_SITES = {
         name: '卧龙资源',
     },
     hwba: {
+        api: 'https://cjwba.com',
+        name: '华为吧资源',
+    },
+        hwba: {
         api: 'https://cjwba.com',
         name: '华为吧资源',
     },
